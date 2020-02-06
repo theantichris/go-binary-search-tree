994dd993cbe02b2617c8daad95bfe0d0a862b76f
Christopher Lamm <chris@theantichris.com>
2020-02-06 08:36:25 -0500

    implemented inserting values into the tree

--- main.go
+++ main.go
@@ -1,5 +1,45 @@
 package main
 
+import "errors"
+
 func main() {
 
+}
+
+// TreeNode represents a node in a binary search tree.
+type TreeNode struct {
+	value int
+	left  *TreeNode
+	right *TreeNode
+}
+
+// Insert will insert a new value into the binary search tree.
+func (treeNode *TreeNode) Insert(value int) error {
+	if treeNode == nil {
+		return errors.New("tree is empty")
+	}
+
+	if treeNode.value == value {
+		return errors.New("this node value already exists")
+	}
+
+	if treeNode.value > value {
+		if treeNode.left == nil {
+			treeNode.left = &TreeNode{value: value}
+			return nil
+		}
+
+		return treeNode.left.Insert(value)
+	}
+
+	if treeNode.value < value {
+		if treeNode.right == nil {
+			treeNode.right = &TreeNode{value: value}
+			return nil
+		}
+
+		return treeNode.right.Insert(value)
+	}
+
+	return nil
 }
